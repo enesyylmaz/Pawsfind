8de0b91f27371402b735d12443cef9ef7e652681
Enes Yılmaz <enesyilmaz102@hotmail.com>
2023-07-02 20:55:05 +0300

    add entry

---
+++ .history/client/src/App_20230702180203.js
@@ -0,0 +1,240 @@
+import GoogleMap from "google-maps-react-markers";
+import { useEffect, useRef, useState } from "react";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import Info from "./info";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import Marker from "./marker";
+import "./style.css";
+import axios from "axios";
+const URL = "http://localhost:4000";
+
+const App = () => {
+  const mapRef = useRef(null);
+  const mapContainerRef = useRef(null);
+  const [mapReady, setMapReady] = useState(false);
+  const [mapBounds, setMapBounds] = useState({});
+  const [highlighted, setHighlighted] = useState(null);
+  const [mapDragged, setMapDragged] = useState(false);
+
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  /**
+   * @description This function is called when the map is ready
+   * @param {Object} map - reference to the map instance
+   * @param {Object} maps - reference to the maps library
+   */
+  // eslint-disable-next-line no-unused-vars
+  const onGoogleApiLoaded = ({ map, maps }) => {
+    mapRef.current = map;
+    setMapReady(true);
+  };
+
+  const onMapChange = ({ bounds, zoom }) => {
+    const ne = bounds.getNorthEast();
+    const sw = bounds.getSouthWest();
+    setMapBounds({
+      ...mapBounds,
+      bounds: [sw.lng(), sw.lat(), ne.lng(), ne.lat()],
+      zoom,
+    });
+  };
+
+  const [coordinateData, setCoordinateData] = useState([]);
+
+  const fetchDataView = () => {
+    fetch(`${URL}/api/pets`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCoordinateData(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchDataView();
+  }, []);
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === "Escape") {
+        setHighlighted(null);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleMapDragStart = () => {
+      setMapDragged(true);
+    };
+
+    const handleMapDragEnd = () => {
+      setMapDragged(false);
+    };
+
+    const mapContainer = mapContainerRef.current;
+
+    if (mapContainer) {
+      mapContainer.addEventListener("mousedown", handleMapDragStart);
+      mapContainer.addEventListener("mouseup", handleMapDragEnd);
+    }
+
+    return () => {
+      if (mapContainer) {
+        mapContainer.removeEventListener("mousedown", handleMapDragStart);
+        mapContainer.removeEventListener("mouseup", handleMapDragEnd);
+      }
+    };
+  }, [mapContainerRef]);
+
+  const onMarkerClick = (e, { markerId, lat, lng }) => {
+    setHighlighted({ markerId, lat, lng });
+
+    const fullscreenElement =
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement;
+
+    if (fullscreenElement) {
+      // Exit fullscreen mode
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    }
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (user && user.access_token) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+          setLoading(false);
+          localStorage.setItem("user", JSON.stringify(user));
+        })
+        .catch((err) => console.log(err));
+    } else {
+      setProfile(null);
+      setLoading(false);
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+      localStorage.setItem("user", JSON.stringify(codeResponse));
+    },
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  const logOut = () => {
+    setUser(null);
+    setProfile(null);
+    setLoading(true);
+    localStorage.removeItem("user");
+    googleLogout();
+  };
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <main>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : profile ? (
+          <div className="profile-container">
+            <div className="profile">
+              <img src={profile.picture} alt="user image" />
+              <h3>User Logged in</h3>
+              <p>Name: {profile.name}</p>
+              <p>Email Address: {profile.email}</p>
+              <button onClick={logOut}>Log out</button>
+              <button>Add Entry</button>
+            </div>
+          </div>
+        ) : (
+          <button className="sign_button" onClick={() => login()}>
+            Sign in with Google 🚀{" "}
+          </button>
+        )}
+      </div>
+      <div className="map-container" ref={mapContainerRef}>
+        <GoogleMap
+          apiKey="AIzaSyCpSbd_GTUT5hRGzW-BBK6mXYX_quZ6ZOQ"
+          defaultCenter={{ lat: 40.377, lng: 28.8832 }}
+          defaultZoom={4}
+          onGoogleApiLoaded={onGoogleApiLoaded}
+          onChange={onMapChange}
+        >
+          {coordinateData.map(({ lat, lng, name }, index) => (
+            <Marker
+              key={index}
+              lat={lat}
+              lng={lng}
+              markerId={name}
+              onClick={onMarkerClick}
+              className="marker"
+            />
+          ))}
+        </GoogleMap>
+      </div>
+      {highlighted && (
+        <div className="popup">
+          <div className="popup-content">
+            <div className="popup-header">
+              <h3>{highlighted.markerId}</h3>
+              <button
+                type="button"
+                onClick={() => setHighlighted(null)}
+                className="close-button"
+              >
+                X
+              </button>
+            </div>
+            <div className="popup-body">
+              <p>Latitude: {highlighted.lat}</p>
+              <p>Longitude: {highlighted.lng}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default App;
---
+++ .history/client/src/index_20230702205359.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider
+      clientId="138088655548-k5nqa71rdrulnbrujpg9be75q1qkp88p.apps.googleusercontent.com"
+      redirectUri="https://pawsfind.onrender.com" // Replace with your redirect URI
+      scopes={["email", "profile"]}
+    >
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
