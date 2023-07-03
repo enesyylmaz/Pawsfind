cbb5b51f0121587605b9a4e55243fd91d781fab7
Enes Yılmaz <enesyilmaz102@hotmail.com>
2023-07-03 18:36:29 +0300

    0.8

---
+++ .history/client/src/marker_20230703155353.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import markerPin1 from "./marker-pin.png";
+//import markerPin2 from "./marker-pin2.png";
+
+const Marker = ({
+  className,
+  lat,
+  lng,
+  markerId,
+  onClick,
+  variant,
+  ...props
+}) => {
+  const markerImage = variant === "variant1" ? markerPin1 : markerPin2;
+
+  return (
+    <img
+      className={className}
+      src={markerImage}
+      lat={lat}
+      lng={lng}
+      onClick={(e) => (onClick ? onClick(e, { markerId, lat, lng }) : null)}
+      style={{ cursor: "pointer", fontSize: 40 }}
+      alt={markerId}
+      {...props}
+    />
+  );
+};
+
+Marker.defaultProps = {
+  variant: "variant1",
+};
+
+Marker.propTypes = {
+  className: PropTypes.string,
+  /**
+   * The id of the marker.
+   */
+  markerId: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    .isRequired,
+  /**
+   * The latitude of the marker.
+   */
+  lat: PropTypes.number.isRequired,
+  /**
+   * The longitude of the marker.
+   */
+  lng: PropTypes.number.isRequired,
+  /**
+   * The function to call when the marker is clicked.
+   */
+  onClick: PropTypes.func,
+  /**
+   * The variant of the marker image.
+   */
+  variant: PropTypes.oneOf(["variant1", "variant2"]),
+};
+
+export default Marker;
